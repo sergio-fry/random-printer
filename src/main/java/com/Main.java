6600b56de9495af5091f5a7a8d07ce7dfc3728fe
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-02-19 08:56:54 +0300

    window

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -6,24 +6,78 @@ import java.io.File;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.layout.VBox;
+
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.printing.PDFPageable;
 
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import java.io.IOException;
 import java.awt.print.PrinterException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Main {
+public class Main extends Application {
 
     static PDDocument document;
+    static TextArea log_renderer;
 
-    public static void main(String[] args) throws InvalidPasswordException, IOException, PrinterException {
-        print();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Random Printer");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
+        Scene scene = new Scene(new Group());
+        VBox root = new VBox();
+
+
+        Button button = new Button();
+        button.setText("Start Print");
+        
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                try {
+                    print();
+                } catch (Exception ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    
+                }
+            }
+        });
+        
+        log_renderer = new TextArea();
+        root.getChildren().addAll(button, log_renderer);
+        log("Idling");
+
+        scene.setRoot(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    private static void print() throws IOException, NullPointerException, PrinterException {
-        document = PDDocument.load(new File("./example.pdf"));
+    //InvalidPasswordException, IOException, PrinterException
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    
+    private static void log(String s) {
+        log_renderer.appendText(s + "\n");
+    }
+
+    private static void print() throws Exception {
+        log("printing");
         
+        document = PDDocument.load(new File("./example.pdf"));
+
         PrintService myPrintService = findPrintService("PDFwriter");
 
         PrinterJob job = PrinterJob.getPrinterJob();
