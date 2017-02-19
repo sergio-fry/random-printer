29bfc83b35dbd1bf41f6063aaab14927356928ab
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-02-19 15:25:01 +0300

    take period from textfield

--- src/main/java/com/randomprinter/Main.java
+++ src/main/java/com/randomprinter/Main.java
@@ -5,13 +5,12 @@ import java.io.File;
 
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
-
+import javafx.application.Platform;
+import javafx.stage.WindowEvent;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.layout.VBox;
@@ -19,22 +18,19 @@ import javafx.scene.layout.VBox;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.printing.PDFPageable;
 
-import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
-import java.io.IOException;
-import java.awt.print.PrinterException;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import java.util.concurrent.TimeUnit;
-import javafx.application.Platform;
-import javafx.stage.WindowEvent;
 
-;
+import java.util.Random;
 
 public class Main extends Application {
 
     static PDDocument document;
     static TextArea log_renderer;
+    static Random randomGenerator;
+    static TextField period_input;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -42,12 +38,9 @@ public class Main extends Application {
         primaryStage.setWidth(500);
         primaryStage.setHeight(500);
         
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent t) {
-                Platform.exit();
-                System.exit(0);
-            }
+        primaryStage.setOnCloseRequest((WindowEvent t) -> {
+            Platform.exit();
+            System.exit(0);
         });
         
         Scene scene = new Scene(new Group());
@@ -56,37 +49,40 @@ public class Main extends Application {
         Button button = new Button();
         button.setText("Start Print");
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                try {
-                    startPrintLoop();
-                } catch (Exception ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    log(ex.getMessage());
-                }
+        button.setOnAction((ActionEvent e) -> {
+            try {
+                startPrintLoop();
+            } catch (Exception ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                log(ex.getMessage());
             }
         });
 
         log_renderer = new TextArea();
-        root.getChildren().addAll(button, log_renderer);
-        log("Idling");
 
+        period_input = new TextField("2000");
+        period_input.setPromptText("2000 means 2 seconds");
+        
+        
+        root.getChildren().addAll(new Label("Period (in millisecods):"), period_input, button, log_renderer);
+        
         scene.setRoot(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    //InvalidPasswordException, IOException, PrinterException
     public static void main(String[] args) throws Exception {
+        randomGenerator = new Random();
         launch(args);
+        log("Idling");
+
     }
 
     private static void log(String s) {
         log_renderer.appendText(s + "\n");
     }
 
-    public static void startPrintLoop() throws InterruptedException {
+    private static void startPrintLoop() throws InterruptedException {
         new java.util.Timer().schedule(
                 new java.util.TimerTask() {
 
@@ -99,7 +95,15 @@ public class Main extends Application {
                     log(ex.getLocalizedMessage());
                 }
             }
-        }, 0, 2000);
+        }, 0, printPeriod());
+    }
+    
+    private static Integer printPeriod() {
+        if ("".equals(period_input.getText())) {
+            return 2000;
+        } else {
+            return Integer.parseInt(period_input.getText());            
+        }
     }
 
     private static void print() throws Exception {
@@ -109,14 +113,16 @@ public class Main extends Application {
 
         PrinterJob job = PrinterJob.getPrinterJob();
         job.setPageable(new PDFPageable(document));
-        job.setPrintService(printService());
+        job.setPrintService(randomPrintService());
         job.print();
     }
 
-    private static PrintService printService() {
+    private static PrintService randomPrintService() {
         PrintService[] printServices = PrintServiceLookup.lookupPrintServices(null, null);
+        
         if (printServices.length > 0) {
-            return printServices[0];
+            int index = randomGenerator.nextInt(printServices.length);
+            return printServices[index];
         }
 
         return null;
